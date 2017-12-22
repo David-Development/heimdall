ec2995626eff1b72e44017e50438e85d26e1c2d3
David Luhmer <david-dev@live.de>
2017-12-22 21:27:02 +0100

    add MQTT support

--- heimdall-camera-proxy/camerahandler.py
+++ heimdall-camera-proxy/camerahandler.py
@@ -11,9 +11,14 @@ HOST = '0.0.0.0'
 PORT = 9000
 
 # Heimdall API URL
-HEIMDALL_BACKEND = os.environ.get('HEIMDALL_BACKEND')
+#HEIMDALL_BACKEND = os.environ.get('HEIMDALL_BACKEND')
 
-print("Heimdall-Backend: {}".format(HEIMDALL_BACKEND))
+### MQTT 
+#broker = 'mqtt-broker' 
+broker = os.environ.get('MQTT_BROKER_IP')
+topic ='trigger' 
+mqttQos = 0
+mqttRetained = False 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -34,6 +39,55 @@ s.listen(10)
 print("Socket now listening on {}:{}".format(HOST, PORT))
 
 
+
+def post_image(image_base_64): 
+    print("============")
+    print('Sending photo') 
+    (result, mid) = client.publish("camera", image_base_64, mqttQos, mqttRetained)
+    print(result, ' - ', mid)
+
+
+def on_connect(client, userdata, flags, rc): 
+    print("Connected with result code "+str(rc)) 
+    client.subscribe(topic) 
+    
+# The callback for when a PUBLISH message is received from the server.
+# 
+def on_message(client, userdata, msg): 
+    print("=========")
+    payload = str(msg.payload.decode('ascii'))  # decode the binary string 
+    print("on_message: " + msg.topic + " " + payload) 
+    process_trigger(payload) 
+
+def on_publish(client, obj, mid):
+    print("=========")
+    print("Publish - Mid:", mid)
+
+def on_log(client, userdata, level, buf):
+    print(level, buf)
+
+def on_disconnect(client, userdata, rc):
+    print("=========")
+    print("Disconnected:", mqtt.connack_string(rc))
+
+
+client = mqtt.Client(client_id="heimdall-camera")  # protocol=mqtt.MQTTv31
+
+client.on_connect = on_connect    # call these on connect and on message 
+client.on_message = on_message
+client.on_publish = on_publish 
+client.on_disconnect = on_disconnect
+#client.on_log = on_log
+
+#client.username_pw_set(username='user',password='pwd')  # need this 
+client.connect(broker, 1883, 60) 
+# client.loop_forever()    #  don't get past this 
+client.loop_start()    #  run in background and free up main thread
+
+
+
+
+
 # Function for handling connections. This will be used to create threads
 def clientthread(conn):
     image = ''
@@ -48,9 +102,8 @@ def clientthread(conn):
     conn.close()
 
     # send image
-    url = HEIMDALL_BACKEND + '/api/live/'
-    data = {'image': base64.b64encode(image.decode("hex")), 'annotate': 'True'}
-    requests.post(url, data=data)
+    image_base_64 = base64.b64encode(image.decode("hex"))
+    post_image(image_base_64)
 
 
 # now keep talking with the client
