e30f342ff2eeb1f21ed67e44a5fe28cbc49b8f42
David Luhmer <david-dev@live.de>
2017-12-25 10:52:51 +0100

    update camera proxy container / add support for MQTT / use socketserver / use timeout for recv operation to prevent blocking if connections die

--- heimdall-camera-proxy/camerahandler.py
+++ heimdall-camera-proxy/camerahandler.py
@@ -5,35 +5,51 @@ import threading
 import requests
 import os
 import codecs
+import paho.mqtt.client as mqtt
+
+import cv2
+import numpy as np
+
+import socketserver
+
+
+
+import multiprocessing.pool
+import functools
+
+def timeout(max_timeout):
+    """Timeout decorator, parameter in seconds."""
+    def timeout_decorator(item):
+        """Wrap the original function."""
+        @functools.wraps(item)
+        def func_wrapper(*args, **kwargs):
+            """Closure for function."""
+            pool = multiprocessing.pool.ThreadPool(processes=1)
+            async_result = pool.apply_async(item, args, kwargs)
+            # raises a TimeoutError if execution exceeds max_timeout
+            return async_result.get(max_timeout)
+        return func_wrapper
+    return timeout_decorator
+
+
+
+
+
+
 
 # Listen on
 HOST = ''
+#HOST = '0.0.0.0'
 PORT = 9000
 
 ### MQTT 
 #broker = 'mqtt-broker' 
 broker = os.environ.get('MQTT_BROKER_IP')
+#broker = 'localhost'
 topic ='trigger' 
 mqttQos = 0
 mqttRetained = False 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-print('Socket created')
-
-# Bind socket to local host and port
-try:
-    print("Binding socket to host...")
-    s.bind((HOST, PORT))
-except socket.error as msg:
-    print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-    sys.exit()
-
-print('Socket bind complete')
-
-# Start listening on socket
-s.listen(10)
-print("Socket now listening on {}:{}".format(HOST, PORT))
 
 
 
@@ -54,7 +70,6 @@ def on_message(client, userdata, msg):
     print("=========")
     payload = str(msg.payload.decode('ascii'))  # decode the binary string 
     print("on_message: " + msg.topic + " " + payload) 
-    process_trigger(payload) 
 
 def on_publish(client, obj, mid):
     print("=========")
@@ -77,43 +92,53 @@ client.on_disconnect = on_disconnect
 #client.on_log = on_log
 
 #client.username_pw_set(username='user',password='pwd')  # need this 
+
+print("Connecting to broker: " + broker)
 client.connect(broker, 1883, 60) 
 # client.loop_forever()    #  don't get past this 
 client.loop_start()    #  run in background and free up main thread
 
 
 
+import time
 
 
-# Function for handling connections. This will be used to create threads
-def clientthread(conn):
-    image = b''
-    while True:
-        # Receiving from client
-        data = conn.recv(4096)
-        if not data:
-            break
-        image += data
+class CameraTCPHandler(socketserver.BaseRequestHandler):
 
-    # came out of loop
-    conn.close()
+    @timeout(2)  # if execution takes longer than X seconds, raise a TimeoutError
+    def handle(self):
 
-    # send image
-    image_base_64 = base64.b64encode(codecs.decode(image, "hex"))
-    post_image(image_base_64)
+        image = b''
+        print('Receiving data..')
+        while True:
+            # Receiving from client
+            data = self.request.recv(4096)
+            if not data:
+                break
+            #print("More data: " + str(sys.getsizeof(data)))
+            image += data
+        print('Done Receiving!')
 
 
-# now keep talking with the client
-while 1:
-    # wait to accept a connection - blocking call
-    conn, addr = s.accept()
-    print('Connected with ' + addr[0] + ':' + str(addr[1]))
+        image = codecs.decode(image, "hex")
 
-    # start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
-    #start_new_thread(clientthread, (conn,))
-    threading.Thread(
-        target=clientthread,
-        args=(conn,),
-    ).start()
+        #nparr = np.fromstring(image, np.uint8)
+        #image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        #_, image = cv2.imencode('.jpg', image)
+        #cv2.imwrite('test.jpg', image)
 
-s.close()
+        # send image
+        image_base_64 = base64.b64encode(image)
+        post_image(image_base_64)
+
+
+
+
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+
+print("Socket listening on {}:{}".format(HOST, PORT))
+server = ThreadedTCPServer((HOST, PORT), CameraTCPHandler)
+server.serve_forever()
