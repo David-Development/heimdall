bf5e3ccff78db38eb6f3215bcfcab09085d2d23a
David <david-dev@live.de>
2018-03-23 10:42:31 +0100

    update updateScript

--- updateHeimdall.sh
+++ updateHeimdall.sh
@@ -7,16 +7,13 @@ fi
 
 docker-compose stop
 
-docker pull luhmer/opencv-python3
-docker pull luhmer/heimdall-backend
-docker pull luhmer/heimdall-frontend
-docker pull luhmer/emqtt
-
 git pull
 git submodule init
 git submodule update --recursive --remote
 
-# workaround yarn permission denied...
-docker-compose up --build heimdall-frontend
+docker pull luhmer/opencv-python3
+docker pull luhmer/heimdall-backend
+docker pull luhmer/heimdall-frontend
+docker pull luhmer/emqtt
 
 # reboot
